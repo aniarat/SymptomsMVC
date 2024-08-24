92ef1d5ce419b7f903c2d173fad60d55c84412b5
aniarat <anka.ratowska@gmail.com>
2024-08-24 11:45:24 +0200

    add mvc project to the dir

---
+++ SymptomsAppMVC/SymptomsAppMVC/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace SymptomsAppMVC.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
